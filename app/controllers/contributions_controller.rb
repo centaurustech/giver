b9886f0cb50e392a6e2b00b4f161f0af6f0a3061
Toby Redd <tobyredd@go.byuh.edu>
2015-03-03 08:18:48 -0800

    commit before messing with angular again

--- app/controllers/contributions_controller.rb
+++ app/controllers/contributions_controller.rb
@@ -1,8 +1,13 @@
 class ContributionsController < ApplicationController
 	def new
 		@contribution = Contribution.new
+		@user = session[:user_id]
+		@event = params[:id]
 	end
 
 	def create
+		puts "\n\n\n\n\n"
+		puts params
+		puts "\n\n\n\n\n"
 	end
 end
