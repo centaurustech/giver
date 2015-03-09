3db3075f45d2e1ffc443c9083aac03d7ec03b0a7
Toby Redd <tobyredd@go.byuh.edu>
2015-03-08 22:16:21 -0700

    added aws initializer as well as paperclip initializer

---
+++ config/initializers/aws.rb
@@ -0,0 +1,4 @@
+AWS.config(access_key_id:     ENV['AKIAJOSMVKQGYHWOIB7Q'],
+           secret_access_key: ENV['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'] )
+
+S3_BUCKET = AWS::S3.new.buckets[ENV['mygiver']]
