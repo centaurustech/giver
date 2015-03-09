6b44bdd552fe3c5401dd92229ff2f78cc1b5bd1a
Toby Redd <tobyredd@go.byuh.edu>
2015-03-09 10:31:45 -0700

    fixed spelling error in application yml

--- config/application.rb
+++ config/application.rb
@@ -8,10 +8,6 @@ Bundler.require(*Rails.groups)
 
 module Giver
   class Application < Rails::Application
-    config.paperclip_defaults = {
-        :storage => :s3,
-        :s3_host_name => 's3-us-west-2.amazonaws.com'
-    }
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
