42dacc4332b8a5625dff9fd7f30c2355cab00640
Toby Redd <tobyredd@go.byuh.edu>
2015-03-08 22:32:32 -0700

    modified aws initializer

--- config/initializers/aws.rb
+++ config/initializers/aws.rb
@@ -1,4 +1,4 @@
-AWS.config(access_key_id:     ENV['AKIAJOSMVKQGYHWOIB7Q'],
-           secret_access_key: ENV['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'] )
+# AWS.config(access_key_id:     ENV['AKIAJOSMVKQGYHWOIB7Q'],
+#            secret_access_key: ENV['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'] )
 
-S3_BUCKET = AWS::S3.new.buckets[ENV['mygiver']]
+# S3_BUCKET = AWS::S3.new.buckets[ENV['mygiver']]
