2be92d576333a9b4e920daf3fa4f73e0009cde83
Toby Redd <tobyredd@go.byuh.edu>
2015-03-09 08:39:36 -0700

    commented out things related to s3

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,7 +3,9 @@ class Item < ActiveRecord::Base
   has_many :event_items
   has_many :events, :through => :event_items
   has_attached_file :photo, :default_url => "/images/missing.png"
-  has_attached_file :photo, :styles => { :medium => "200x150>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png", :storage => :s3, :s3_credentials => "#{RAILS_ROOT}/config/environments/production.rb", :path => "giver/:attachment/:style/:id.:extension"
+  has_attached_file :photo, :styles => { :medium => "200x150>", :thumb => "100x100>" }
+
+  # , :default_url => "/images/:style/missing.png", :storage => :s3, :s3_credentials => "#{RAILS_ROOT}/config/environments/production.rb", :path => "giver/:attachment/:style/:id.:extension"
   validates_attachment_content_type :photo, :content_type => /\Aimage\/.*\Z/
   validates :photo, :attachment_presence => true
   validates :item_name, :total_price, :board_id, :photo, presence: true
